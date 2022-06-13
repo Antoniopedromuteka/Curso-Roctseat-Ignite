f8a6f7dc6207414f9af9298d14155db27746f578
Antoniopedromuteka <90418160+Antoniopedromuteka@users.noreply.github.com>
2022-06-13 16:05:48 +0100

    consumindo api com o axios

--- Capitulo2/dtmoney/src/Components/TransactionTable/index.tsx
+++ Capitulo2/dtmoney/src/Components/TransactionTable/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./style";
 
 import { useEffect } from "react";
+import { api } from "../../services/api";
 
 
 
@@ -8,10 +9,8 @@ export function TransactionTable(){
 
     useEffect(()=>{
 
-        fetch('http://localhost:3000/api/transactions')
-        .then(response => response.json())
-        .then(data => console.log(data))
-
+        api.get('/transactions')
+        .then(response => console.log(response.data))
     }, [])
 
     return(
---
+++ Capitulo2/dtmoney/src/services/api.ts
@@ -0,0 +1,11 @@
+
+
+import axios from "axios";
+
+
+
+export const api = axios.create({
+    
+    baseURL: "http://localhost:3000/api",
+    
+})
