22f9d0663c4ebd6703d37553b52db3052b2ff689
Antoniopedromuteka <90418160+Antoniopedromuteka@users.noreply.github.com>
2022-06-13 22:47:05 +0100

    adicionando o Modal apartir da Biblioteca react Modal

--- Capitulo2/dtmoney/src/App.tsx
+++ Capitulo2/dtmoney/src/App.tsx
@@ -1,17 +1,36 @@
 
 
 import {GlobalStyle} from '../src/styles/global';
+import Modal from 'react-modal';
 import { Header} from './Components/Header';
 import { Dashboard } from './Components/Dashboard';
+import {useState} from 'react';
  
 
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
   return (
     <>
     
-      <Header/>
+      <Header onOpenNewTransitionModal={handleOpenTransactionModal}/>
 
       <Dashboard/>
+
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal}>
+                <h2>Cadastrar Transação</h2>
+      </Modal>
 
       <GlobalStyle/>
     </>  
--- Capitulo2/dtmoney/src/Components/Header/index.tsx
+++ Capitulo2/dtmoney/src/Components/Header/index.tsx
@@ -1,21 +1,32 @@
 
 
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
+
+ 
 
 import { Container, Content } from './styles';
 
+interface HeaderProps{
+    onOpenNewTransitionModal: ()=> void;
+}
 
-export function Header(){
+export function Header({onOpenNewTransitionModal}:HeaderProps){
+
+    
+
     return(
         <Container>
 
             <Content> 
             <img src={logoImg} alt="dt money"></img>
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransitionModal}>
                 Nova Transação
             </button>
+
+           
             </Content>
 
-        </Container>
+        </Container> 
     );
 }
