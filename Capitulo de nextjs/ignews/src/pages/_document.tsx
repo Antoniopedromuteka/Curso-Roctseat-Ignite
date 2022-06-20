b448580372b06776a3ad7f3bc8e75819bcf0e82c
Antoniopedromuteka <90418160+Antoniopedromuteka@users.noreply.github.com>
2022-06-20 12:42:26 +0100

    adicionando fonte roboto

---
+++ Capitulo de nextjs/ignews/src/pages/_document.tsx
@@ -0,0 +1,32 @@
+
+import Document, {Html, Head, Main, NextScript} from "next/document";
+
+
+export default class MyDocument extends Document {
+    render() {
+        return(
+
+            <Html lang="en">
+
+                <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@700;900&display=swap" rel="stylesheet"/>
+                <title>Ignews</title>
+
+              
+                </Head>
+
+                <body>
+                    <Main>
+
+                    </Main>
+
+                    <NextScript/>
+                </body>
+
+            </Html>
+
+        )
+    }
+}
