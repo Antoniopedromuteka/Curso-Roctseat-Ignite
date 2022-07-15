24d35c534e58b7b17f6114fe6bb7fed5c614769d
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-07-15 14:29:26 +0100

    criando o componente home

--- Capitulo de nextjs/ignews/src/pages/index.tsx
+++ Capitulo de nextjs/ignews/src/pages/index.tsx
@@ -2,18 +2,31 @@
 import Head from 'next/head';
 
 
+import styles from './home.module.scss';
+
+
 export default function Home() {
   return (
 
     <>
        <Head>
-        <title>Inicio | Ignews</title>
+        <title>HOME | Ignews</title>
       </Head>
-    <h1>
 
-      Hello  World
-      </h1>
 
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span>world.</h1>
+
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
      </> 
   )
 }
