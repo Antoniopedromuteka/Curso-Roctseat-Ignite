fbc3dfeb82d1457af09916c40599b6aa1fd0c3bd
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-08-04 23:10:26 +0100

    finalizando a pagina de Sign in

--- Capitulo IV/dashgo/src/pages/index.tsx
+++ Capitulo IV/dashgo/src/pages/index.tsx
@@ -1,11 +1,83 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
+import {Flex, Input, Button, Stack, FormLabel, FormControl} from "@chakra-ui/react";
  
 
 const Home: NextPage = () => {
   return (
-   <h1>Dashgo</h1>
+   <Flex 
+        w="100vw"
+        h="100vh"
+        align={"center"}
+        justify={"center"}
+    >
+      
+      <Flex
+        as={"form"}
+        w={"100%"}
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+      <Stack spacing={4}>
+
+
+      <FormControl>
+        <FormLabel htmlFor='email'>Email</FormLabel>
+
+      <Input
+          name='email'
+          type="email"
+          id='email'
+          focusBorderColor='pink.500'
+          bgColor={"900"}
+          variant="filled"
+          _hover={{
+            bgColor:"gray.900" 
+          }}
+          size={"lg"}
+          
+       />
+       </FormControl>
+
+
+       <FormControl>   
+      <FormLabel htmlFor='password'>Senha</FormLabel>
+      <Input
+          name='password'
+          type="password"
+          id='password'
+          focusBorderColor='pink.500'
+          bgColor={"900"}
+          variant="filled"
+          _hover={{
+              bgColor:"gray.900" 
+          }}
+          size={"lg"}
+       />
+
+       </FormControl>
+       </Stack>
+       
+
+      
+
+        <Button
+          type="submit"
+          mt={6}
+          colorScheme={"pink"}
+          size={"lg"}
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+
+   </Flex>
   )
 }
 
